7f7e5c62246c1c0743337e987835b0a005f66445
ssurineni <ssurineni@salesforce.com>
2025-07-05 09:45:24 +0530

    feat: add ESLint configuration and ignore file
    - Added a new ESLint configuration file (.eslintrc.json) to enforce coding standards using Airbnb and Prettier.
    - Created an .eslintignore file to exclude the build directory from linting.
    - Updated package.json to include necessary ESLint dependencies and adjusted versions.
    - Modified pnpm-lock.yaml to reflect the new dependencies and their versions.

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
-chrome.action.onClicked.addListener(function (tab) {
-  chrome.tabs.query({ currentWindow: true }, function (tabs) {
+chrome.action.onClicked.addListener(function handleActionClick(tab) {
+  chrome.tabs.query({ currentWindow: true }, function handleTabsQuery(tabs) {
     const tabsToClose = tabs.filter(
       (t) => !t.pinned && t.groupId === -1 && t.id !== tab.id
-    );
+    )
     if (tabsToClose.length > 0) {
-      chrome.tabs.remove(tabsToClose.map((t) => t.id));
+      chrome.tabs.remove(tabsToClose.map((t) => t.id))
     }
-  });
-}); 
+  })
+})
