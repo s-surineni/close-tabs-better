9b969a576e290e937802ed36cb316949fab3d58d
ssurineni <ssurineni@salesforce.com>
2025-07-06 10:58:34 +0530

    feat: add Options component for user settings

---
+++ options.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+function Options() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: 32,
+        fontFamily: "sans-serif"
+      }}>
+      Options page loaded!
+    </div>
+  )
+}
+
+export default Options
