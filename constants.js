60a6144daf1474b81ace0ff426fcfcdd2555b66b
ssurineni <ssurineni@salesforce.com>
2025-11-14 22:52:47 +0530

    refactor: create constants file for shared constants
    - Introduced a new constants.js file to define shared constants for inactivity timeout and protected domains.

---
+++ constants.js
@@ -0,0 +1,5 @@
+// Shared constants for Close Tabs Better extension
+export const STORAGE_KEY_INACTIVITY_TIMEOUT_MINUTES = "inactivityTimeoutMinutes"
+export const STORAGE_KEY_PROTECTED_DOMAINS = "protectedDomains"
+export const DEFAULT_TIMEOUT_MINUTES = 120 // 2 hours default
+
